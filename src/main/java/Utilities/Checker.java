993566f3f59ae371a5487f93c879f85747b5d3a3
nosedhd <jose.cruz1@udea.edu.co>
2023-04-05 13:27:57 -0500

    IntChecker
    ola

---
+++ src/main/java/Utilities/Checker.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utilities;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Checker {
+    /* El método en si no va a hacer que se vuelva a preguntar puesto que eso 
+     * se debe hacer dentro del codigo de donde se va a llamar el IntChecker
+     * eso se logra con un ciclo while que vaya preguntando por cada iteración
+     * que tenga al método como variable iteradora.
+    */
+    public static boolean IntChecker(String str) { // Este método sirve para validar si el String ingresado es Integer o String.
+        try {
+            Integer.parseInt(str);
+            return true; // Retorna true si el String puede ser cambiado en su totalidad en Integer.
+        } catch (NumberFormatException e) {
+            return false; // En caso contrario, retorna false.
+        }
+    }
+}
